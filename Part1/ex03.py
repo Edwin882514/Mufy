eb51c0a972674acf17db989311aa5089ac839f76
Edwin882514 <edwinhii0714@gmail.com>
2026-05-25 05:18:16 +0000

    first python programminh class

---
+++ Part1/ex03.py
@@ -0,0 +1,8 @@
+# Original text
+string1 = 'MaRviN THE bOt'
+
+
+print(string1)
+print(string1.casefold())
+print(string1.upper())
+print(string1.replace("MaRviN THE bOt","MaRviNTHEbOt"))
---
+++ Part1/ex05.py
@@ -0,0 +1,7 @@
+word1 = 'car '
+word2 = 'exceeded '
+text1 = 'speed limit '
+word3 = 'the '
+
+result = word3 + word1 + word2 + word3 + text1
+print(result)
